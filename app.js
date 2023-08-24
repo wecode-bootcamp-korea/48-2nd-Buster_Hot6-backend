4de6337acd62b4e6bdcc73b682b00190072413f8
kimmangyu <vlsual0917@gmail.com>
2023-08-24 19:00:25 +0900

    Fixed Migration

--- app.js
+++ app.js
@@ -2,15 +2,15 @@ require("dotenv").config();
 const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
-const { AppDataSource } = require("./src/models/dataSource");
-const {routes} = require("./src/routes")
+const { dataSource } = require("./src/models/dataSource");
+
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 app.use(morgan('combined'));
-app.use(routes);
+
 
 app.get("/ping", (req, res, next) => {
   res.status(200).json({ message: "pong" });
@@ -19,7 +19,7 @@ app.get("/ping", (req, res, next) => {
 const startServer = async () => {
   const PORT = process.env.PORT;
 
-  await AppDataSource.initialize();
+  await dataSource.initialize();
 
   app.listen(PORT, () => {
     console.log(`Listening on Port ${PORT}`);
