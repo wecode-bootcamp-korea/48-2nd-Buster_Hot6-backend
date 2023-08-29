3c5b99906e6549100ad54b156ef281e482f0cd59
KCH <130543414+kkkkcccchhhh@users.noreply.github.com>
2023-08-29 21:35:05 +0900

    1차 수정 미완성

--- src/routes/productScrapsRouter.js
+++ src/routes/productScrapsRouter.js
@@ -2,9 +2,9 @@ const express = require("express");
 
 const productScrapsController = require("../controllers/productScrapsController");
 const productScrapsRouter = express.Router();
-const { loginRequired } = require('../utils/auth')
+const {loginRequired} = require('../utils/auth')
 
-productScrapsRouter.post("/scrapOn", loginRequired, productScrapsController.productScrapon);
-productScrapsRouter.delete("/scrapOff", loginRequired, productScrapsController.productScrapoff);
+productScrapsRouter.post("/scrapOn", loginRequired, productScrapsController.scrapon);
+productScrapsRouter.delete("/scrapOff", loginRequired, productScrapsController.scrapoff);
 
 module.exports = { productScrapsRouter };
