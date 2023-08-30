f800825b0c5a3e49f5dec755d463312ae4669d3a
KCH <130543414+kkkkcccchhhh@users.noreply.github.com>
2023-08-30 14:34:05 +0900

    confilct 수정완료

--- src/routes/userRouter.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const userController = require("../controllers/userControllers");
-const userRouter = express.Router();
-
-userRouter.post('/signup', userController.signUp);
-userRouter.post('/signin', userController.signIn);
-
-module.exports = { userRouter };
