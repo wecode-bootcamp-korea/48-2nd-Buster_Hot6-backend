aa76e5bc5617ca6176d2d68be25292eedef5543d
홍이 <cuihong928@gmail.com>
2023-08-30 17:57:57 +0900

    scrap 기능 수정

--- src/controllers/postScrapControllers.js
+++ src/controllers/postScrapControllers.js
@@ -2,15 +2,9 @@ const { catchAsync } = require("../utils/error");
 const postScrapService = require("../services/postScrapServices");
 
 const postScrapCountByPostId = catchAsync(async (req, res) => {
-  try {
-    const { postId } = req.query;
-    /*  console.log(req.query);
-    console.log(postId); */
-    const count = await postScrapService.getPostScrapCountByPostId(postId);
-    res.status(200).json({ scrapCount: count });
-  } catch (err) {
-    res.status(err.statusCode || 401).json({ message: err.message });
-  }
+  const { postId } = req.query;
+  const count = await postScrapService.getPostScrapCountByPostId(postId);
+  res.status(200).json({ scrapCount: count });
 });
 
 module.exports = { postScrapCountByPostId };
--- src/services/productsServices.js
+++
@@ -1,7 +0,0 @@
-const { getProductsByCategoryId } = require('../models/productsDao');
-
-const productService = async (categoryId, offset, limit) => {
-  return await getProductsByCategoryId(categoryId, offset, limit);
-};
-
-module.exports = { productService };
