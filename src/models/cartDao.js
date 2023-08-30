b7ce35e94fd3870c41b32a87dbd8c1124fb316fc
김시연 <kimsiyeon@MacBook-Air.local>
2023-08-30 15:34:44 +0900

    save

--- src/models/cartDao.js
+++ src/models/cartDao.js
@@ -17,7 +17,7 @@ const insertCart = async (user_id, product_id, product_count) => {
       [user_id, product_id, product_count]
     );
   } catch (eroor) {
-    const error = new Error("DataSource ERROR CategoryId");
+    const error = new Error("DataSource ERROR");
     error.statusCode = 401;
   }
 };
@@ -32,9 +32,26 @@ const deleteCart = async (user_id, product_id) => {
       [user_id, product_id]
     );
   } catch (eroor) {
-    const error = new Error("DataSource ERROR CategoryId");
+    const error = new Error("DataSource ERROR");
     error.statusCode = 401;
   }
 };
 
-module.exports = { insertCart, deleteCart };
+const getCartList = async (user_id) => {
+  try {
+    await AppDataSource.query(
+      `
+    SELECT c.id, c.user_id, c.product_id, c.product_count
+    FROM carts c
+    JOIN users u ON c.user_id = u.id
+    WHERE c.user_id = ?
+    `,
+      [user_id]
+    );
+  } catch (eroor) {
+    const error = new Error("DataSource ERROR");
+    error.statusCode = 401;
+  }
+};
+
+module.exports = { insertCart, deleteCart, getCartList };
