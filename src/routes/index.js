4040a19fadced6939398b900b8143554428342ed
KCH <130543414+kkkkcccchhhh@users.noreply.github.com>
2023-08-30 12:41:10 +0900

    scrap브랜치 파일 삭제 완료

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,14 +1,10 @@
 const express = require("express");
 
 const { userRouter } = require("./userRouter")
-const { productScrapsRouter } = require("./productScrapsRouter")
-const { postScrapsRouter } = require("./postScrapsRouter")
 const { reviewsRouter } = require("./reviewsRouter")
 const routes = express.Router();
 
 routes.use("/users", userRouter);
-routes.use("/productScrap", productScrapsRouter);
-routes.use("/postScrap", postScrapsRouter);
 routes.use("/reviews", reviewsRouter);
 
 module.exports = { routes } ;
