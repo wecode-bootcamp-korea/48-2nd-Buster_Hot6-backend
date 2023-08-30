49cda6519641797e3601b257b7534a783ddb7352
KCH <130543414+kkkkcccchhhh@users.noreply.github.com>
2023-08-30 14:17:22 +0900

    conflict 문제 해결 수정' ;

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,8 @@
 const express = require("express");
 
-const { userRouter } = require("./userRouter")
 const { reviewsRouter } = require("./reviewsRouter")
 const routes = express.Router();
 
-routes.use("/users", userRouter);
 routes.use("/reviews", reviewsRouter);
 
 module.exports = { routes } ;
