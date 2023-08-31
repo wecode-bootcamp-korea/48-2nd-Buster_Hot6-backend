6c3a2937c028e1c0c1ff83c528f7c9101e903e26
KCH <130543414+kkkkcccchhhh@users.noreply.github.com>
2023-08-31 13:36:58 +0900

    마무리 수정13:30

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,10 +5,11 @@ const routes = express.Router();
 const { userRouter } = require("./userRouter");
 const { productRouter } = require("./productsRouter");
 const { cartRouter } = require("./cartRouter");
-const { postRouter } = require("./postsRouter")
+const { postsRouter } = require("./postsRouter")
 
 routes.use("/users", userRouter);
 routes.use("/products", productRouter);
 routes.use("/cart", cartRouter);
+routes.use("/posts", postsRouter)
 
 module.exports = { routes };
